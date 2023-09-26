9ca04ffaef0f71b4b00063939844ff477fe1abae
“[Mohammad <you@example.com>
2023-09-26 13:03:42 -0500

    updated to v18

--- src/components/FeedbackItem.jsx
+++ src/components/FeedbackItem.jsx
@@ -4,10 +4,18 @@ function FeedbackItem() {
   const [rating, setRating] = useState(0);
   const [text, setText] = useState("This is an example of Feedback text");
 
+  const handleClick = () => {
+    setRating((prev) => {
+      console.log(prev);
+      return prev + 1;
+    });
+  };
+
   return (
     <div className="card">
       <div className="num-display">{rating}</div>
       <div className="text-display">{text}</div>
+      <button onClick={handleClick}>click</button>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,12 +1,8 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+const root = createRoot(document.getElementById("root"));
 
-  document.getElementById("root")
-);
+root.render(<App />);
