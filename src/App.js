f8e87da59a24e86ce3a1039d4209c7f7a3872c31
“[Mohammad <you@example.com>
2023-09-26 11:07:19 -0500

    created components folder, added Header.jsx as a component. learned about propTypes, defaultProps, how props work

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
+import Header from "./components/Header";
+
 function App() {
   return (
     <>
-      <h1>Hello from the App component</h1>
+      {/* <h1>Hello from the App component</h1> */}
+      <div className="container"></div>
+      <Header text="hello" />
+      <Header />
     </>
   );
 }
