797c8b0ee4623af40970fdfe2d37ad71d4080502
“[Mohammad <you@example.com>
2023-09-26 11:14:55 -0500

    added global styles from github

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,20 +1,9 @@
-import PropTypes from "prop-types";
-
 function Header({ text }) {
   return (
     <>
       <header>My App</header>
-      <h1>{text}</h1>
     </>
   );
 }
-
-Header.defaultProps = {
-  text: "This is default text",
-};
-
-Header.propTypes = {
-  text: PropTypes.string,
-};
 
 export default Header;
