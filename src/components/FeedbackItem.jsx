9ecd553e9d98ab398de0d2b67528e4a1439cb1e6
“[Mohammad <you@example.com>
2023-09-27 11:26:56 -0500

    updated FeedbackItem.jsx

--- src/components/FeedbackItem.jsx
+++ src/components/FeedbackItem.jsx
@@ -1,21 +1,13 @@
 import { useState } from "react";
 
 function FeedbackItem() {
-  const [rating, setRating] = useState(0);
+  const [rating, setRating] = useState(7);
   const [text, setText] = useState("This is an example of Feedback text");
-
-  const handleClick = () => {
-    setRating((prev) => {
-      console.log(prev);
-      return prev + 1;
-    });
-  };
 
   return (
     <div className="card">
       <div className="num-display">{rating}</div>
       <div className="text-display">{text}</div>
-      <button onClick={handleClick}>click</button>
     </div>
   );
 }
